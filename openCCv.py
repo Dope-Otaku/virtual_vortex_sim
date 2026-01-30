4eb0ba99363fe81ecc53adfdb8fcda99b150e70a
Souvik Halder <fakeyt909@gmail.com>
2026-01-30 17:22:08 +0530

    learned about shape and changing it to grayscale version

--- openCCv.py
+++ openCCv.py
@@ -3,9 +3,16 @@ import cv2
 
 image = cv2.imread('pop_kaal.jpg', 1)
 
+#if image shape
+# print(image.shape)
+
 if image is not None:
-    success =  cv2.imwrite('hello.jpg', image)
-    print("Image saved succesfully")
+    h, w, c = image.shape
+    print(f"Height: {h}\nWidth: {w}\nColor Channel: {c}")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Grayscale version", gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 else:
     print("Image save failed.")
 
