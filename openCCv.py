6aefe8cc5c26953b3700be4a615cd3a7cb36ecf3
Souvik Halder <fakeyt909@gmail.com>
2026-01-30 19:57:08 +0530

    learned how to put text in an image

--- openCCv.py
+++ openCCv.py
@@ -14,6 +14,7 @@ if image is not None:
     w = image.shape[1]
     center = (w//2, h//2)
     radius = 1000
+    text = "hello bhai"
     # print(h, w)
     # print(f"Height: {h}\nWidth: {w}\nColor Channel: {c}")
     # m = cv2.getRotationMatrix2D(center,90,1.0)
@@ -22,6 +23,7 @@ if image is not None:
     # modifiedImage = cv2.line(image, (100,50), (500,50), 0xfff, 5)
     # modifiedImage = cv2.rectangle(image, (100,50), (500,100), 0xfff, 5)
     modifiedImage = cv2.circle(image, center, radius, 0xfff, 5)
+    modifiedImage = cv2.putText(image, text, (200,300), cv2.FONT_HERSHEY_SIMPLEX, 1.2, 0xfff, 5)
     cv2.imshow("original image", image)
     cv2.imshow("new image", modifiedImage)
     # cv2.imshow("rotated image", rotatedImage)
