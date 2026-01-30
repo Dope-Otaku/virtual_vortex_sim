12af8812370c090154ffe6d072e9ddb6bd0e2b16
Souvik Halder <fakeyt909@gmail.com>
2026-01-30 17:53:57 +0530

    learned how we actually cropped images using slicing

--- openCCv.py
+++ openCCv.py
@@ -7,10 +7,12 @@ image = cv2.imread('pop_kaal.jpg', 1)
 # print(image.shape)
 
 if image is not None:
-    resizedImage = cv2.resize(image, (400,400))
-    h, w, c = resizedImage.shape
+    # resizedImage = cv2.resize(image, (400,400))
+    cropped = image[300:900, 500:1040]
+    h, w, c = cropped.shape
     print(f"Height: {h}\nWidth: {w}\nColor Channel: {c}")
-    cv2.imshow("resized image", resizedImage)
+    cv2.imshow("original image", image)
+    cv2.imshow("cropped image", cropped)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
