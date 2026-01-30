e8710a773925445fe83dde68e0408b845f56472c
Souvik Halder <fakeyt909@gmail.com>
2026-01-30 19:21:19 +0530

    learned how to create circles

--- openCCv.py
+++ openCCv.py
@@ -10,16 +10,18 @@ if image is not None:
     # resizedImage = cv2.resize(image, (400,400))
     # cropped = image[300:900, 500:1040]
     # h, w, c = image.shape
-    # h = image.shape[0]
-    # w = image.shape[1]
-    # center = (w//2, h//2)
+    h = image.shape[0]
+    w = image.shape[1]
+    center = (w//2, h//2)
+    radius = 1000
     # print(h, w)
     # print(f"Height: {h}\nWidth: {w}\nColor Channel: {c}")
     # m = cv2.getRotationMatrix2D(center,90,1.0)
     # rotatedImage = cv2.warpAffine(image, m, (w,h))
     # flippedImage = cv2.flip(rotatedImage, -1)
     # modifiedImage = cv2.line(image, (100,50), (500,50), 0xfff, 5)
-    modifiedImage = cv2.rectangle(image, (100,50), (500,100), 0xfff, 5)
+    # modifiedImage = cv2.rectangle(image, (100,50), (500,100), 0xfff, 5)
+    modifiedImage = cv2.circle(image, center, radius, 0xfff, 5)
     cv2.imshow("original image", image)
     cv2.imshow("new image", modifiedImage)
     # cv2.imshow("rotated image", rotatedImage)
