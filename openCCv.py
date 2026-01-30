a599a33896b6bf9738fcadd7519cde6881db4658
Souvik Halder <fakeyt909@gmail.com>
2026-01-30 14:44:41 +0530

    added imwrite function to save images

--- openCCv.py
+++ openCCv.py
@@ -3,7 +3,9 @@ import cv2
 
 image = cv2.imread('pop_kaal.jpg', 1)
 
-if image is None:
-    print("Error: Could not load image.")
+if image is not None:
+    success =  cv2.imwrite('hello.jpg', image)
+    print("Image saved succesfully")
 else:
-    print("Image loaded successfully.")
+    print("Image save failed.")
+
