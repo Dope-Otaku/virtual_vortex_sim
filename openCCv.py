e742915154e520ed6b7760e17f442e8ac4e97cf8
Souvik Halder <fakeyt909@gmail.com>
2026-01-31 22:01:44 +0530

    leanred how to use gaussian blur function

--- openCCv.py
+++ openCCv.py
@@ -1,32 +1,13 @@
 import cv2
 
 
-video = cv2.VideoCapture(1)
+image = cv2.imread("pop_kaal.jpg")
+blurred_image = cv2.GaussianBlur(image, (7,7), 0)
 
-frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+if image is None:
+    print("no image found")
 
-codec = cv2.VideoWriter_fourcc(*'mp4v')
-recordedFootage = cv2.VideoWriter("my_video.mp4", codec, 30, (frame_width,frame_height))
-
-#if image shape
-# print(image.shape)
-
-while True:
-    ret, frame = video.read()
-    # print("press q to quit the window.....") don't use it here it will flood the console
-
-    if not ret:
-        print("could not read frame")
-        break
-
-    recordedFootage.write(frame)
-    cv2.imshow("Recording Live", frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        print("quitting...")
-        break
-
-video.release()
-recordedFootage.release()
+cv2.imshow("original Image", image)
+cv2.imshow("blurred Image", blurred_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
