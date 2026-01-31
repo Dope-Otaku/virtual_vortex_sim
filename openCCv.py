eaf67bad520250d29e2b72211d9e030d30c0657f
Souvik Halder <fakeyt909@gmail.com>
2026-01-31 22:23:07 +0530

    learned about sharpeneing filters

--- openCCv.py
+++ openCCv.py
@@ -1,13 +1,19 @@
 import cv2
+import numpy as np
 
 
 image = cv2.imread("pop_kaal.jpg")
-blurred_image = cv2.medianBlur(image, 21)
+sharpened_kernel = np.array([
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+])
+sharepened_image = cv2.filter2D(image, -1, sharpened_kernel)
 
 if image is None:
     print("no image found")
 
 cv2.imshow("original Image", image)
-cv2.imshow("blurred Image", blurred_image)
+cv2.imshow("blurred Image", sharepened_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
