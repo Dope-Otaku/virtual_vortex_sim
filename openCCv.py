a6a6fb312435a8903a82d836cf7ee9666bcf5c60
Souvik Halder <fakeyt909@gmail.com>
2026-02-01 12:38:15 +0530

    learned about detection of countours in an image

--- openCCv.py
+++ openCCv.py
@@ -14,9 +14,26 @@ if not contours:
 
 cv2.drawContours(image, contours, -1, 0xfff, 3)
     
-# image = cv2.imshow("original image", image)
-modified_image = cv2.imshow("Modified Image", image)
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, (0.01*cv2.arcLength(contour, True)), True)
+    corners=len(approx)
 
+    if corners == 3:
+        shape_name = "Triangle"
+    elif corners == 4:
+        shape_name = "Rectangle"
+    elif corners == 5:
+        shape_name = "Pentagon"
+    elif corners > 5:
+        shape_name = "Circle"
+    else:
+        shape_name = "Unknown"
 
+    cv2.drawContours(image, [approx], 0, 0xfff, 3)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 10
+    cv2.putText(image, shape_name, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, 0xfff, 5)
+
+cv2.imshow("Countours image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
