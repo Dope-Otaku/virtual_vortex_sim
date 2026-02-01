113d176cad8c059f786117d20a49ded84321edf2
Souvik Halder <fakeyt909@gmail.com>
2026-02-01 11:35:25 +0530

    learned about bitwise operations on images

--- openCCv.py
+++ openCCv.py
@@ -1,14 +1,27 @@
 import cv2
 import numpy as np
 
+x= 300
+y= 300
 
-image = cv2.imread("pop_kaal.jpg", cv2.IMREAD_GRAYSCALE)
+xCenter = int(x//2)
+yCenter = int(y//2)
 
-edges = cv2.Canny(image, 50, 150)
-ret, threshold_image = cv2.threshold(image, 120, 255, cv2.THRESH_BINARY)
+img1 = np.zeros((x,y), dtype="uint8")
+img2 = np.zeros((x,y), dtype="uint8")
 
-cv2.imshow("original Image", image)
-# cv2.imshow("blurred Image", edges)
-cv2.imshow("threshold Image", threshold_image)
+circle = cv2.circle(img1, (xCenter, yCenter), (xCenter-50), 255, -1)
+rectangle = cv2.rectangle(img2, (100, 100), (250, 250),255,-1)
+
+
+bitwise_and = cv2.bitwise_and(img1, img2)
+bitwise_or = cv2.bitwise_or(img1, img2)
+bitwise_not = cv2.bitwise_not(img1)
+
+cv2.imshow("Circle", circle)
+cv2.imshow("rectangle", rectangle)
+cv2.imshow("bitwise_and", bitwise_and)
+cv2.imshow("bitwise_or", bitwise_or)
+cv2.imshow("bitwise_not", bitwise_not)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
