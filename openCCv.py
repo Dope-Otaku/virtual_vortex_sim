508419a7958e39012dbc5eae2c77871dbfada2f8
Souvik Halder <fakeyt909@gmail.com>
2026-02-01 12:09:20 +0530

    learned about drawing edges/contours on images

--- openCCv.py
+++ openCCv.py
@@ -1,27 +1,22 @@
 import cv2
 import numpy as np
 
-x= 300
-y= 300
+image = cv2.imread("tri.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-xCenter = int(x//2)
-yCenter = int(y//2)
-
-img1 = np.zeros((x,y), dtype="uint8")
-img2 = np.zeros((x,y), dtype="uint8")
-
-circle = cv2.circle(img1, (xCenter, yCenter), (xCenter-50), 255, -1)
-rectangle = cv2.rectangle(img2, (100, 100), (250, 250),255,-1)
+_, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
 
 
-bitwise_and = cv2.bitwise_and(img1, img2)
-bitwise_or = cv2.bitwise_or(img1, img2)
-bitwise_not = cv2.bitwise_not(img1)
+contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow("Circle", circle)
-cv2.imshow("rectangle", rectangle)
-cv2.imshow("bitwise_and", bitwise_and)
-cv2.imshow("bitwise_or", bitwise_or)
-cv2.imshow("bitwise_not", bitwise_not)
+if not contours:
+    print("countours failed to run")
+
+cv2.drawContours(image, contours, -1, 0xfff, 3)
+    
+# image = cv2.imshow("original image", image)
+modified_image = cv2.imshow("Modified Image", image)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
